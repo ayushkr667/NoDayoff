0f60a987c60c22e7578f14fc3f9e43d19c70c962
Ayush kumar <ayushkr667@gmail.com>
2021-06-03 21:47:09 +0530

    Create Palindrome Linked List.cpp

---
+++ Day4/Palindrome Linked List.cpp
@@ -0,0 +1,29 @@
+//Given a singly linked list node whose values are integers, 
+//determine whether the linked list forms a palindrome.
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * class LLNode {
+ *     public:
+ *         int val;
+ *         LLNode *next;
+ * };
+ */
+bool solve(LLNode* node) {
+    LLNode* curr = node;
+    vector<int> temp;
+    while( curr != NULL ){
+        temp.push_back(curr->val);
+        curr = curr->next;
+    }
+    int i = temp.size() - 1;
+    while(node != NULL){
+        if( node->val != temp[i] ){
+            return false;
+        }
+        i--;
+        node = node->next;
+    }
+    return true;
+}
