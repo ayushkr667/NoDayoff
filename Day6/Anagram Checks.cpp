6b34838af04de4497a6fdf4abc712f9d6b0e1a38
Ayush kumar <ayushkr667@gmail.com>
2021-06-06 16:48:10 +0530

    Create Anagram Checks.cpp

---
+++ Day6/Anagram Checks.cpp
@@ -0,0 +1,11 @@
+// Given two strings s0 and s1, return whether they are anagrams of each other.
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+bool solve(string s0, string s1) {
+    sort(s0.begin(), s0.end());
+    sort(s1.begin(), s1.end());
+    if( s0 == s1)
+        return true;
+    return false;
+}
