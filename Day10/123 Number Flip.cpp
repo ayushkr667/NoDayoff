082f1e6812e918e3ebb6aac3736ef9e2420c454e
Ayush kumar <ayushkr667@gmail.com>
2021-06-09 14:53:18 +0530

    Update 123 Number Flip.cpp

--- Day10/123 Number Flip.cpp
+++ Day10/123 Number Flip.cpp
@@ -1,7 +1,7 @@
 //You are given an integer n consisting of digits 1, 2, 
 //and 3 and you can flip one digit to a 3. Return the maximum number you can make.
 
-
+////////////////////////////////////////////////////////////////////////////////////////
 int solve(int n) {
     vector<int>vec;
     while(n != 0){
